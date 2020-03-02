47c6a06f7bec328043a7b25ea47c7981f70a65e2
Lonitch <invictumltd@gmail.com>
2020-03-02 10:29:28 -0600

    3/2/20

---
+++ 836.rectangle-overlap.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=836 lang=python3
+#
+# [836] Rectangle Overlap
+#
+# https://leetcode.com/problems/rectangle-overlap/description/
+#
+# algorithms
+# Easy (47.72%)
+# Likes:    531
+# Dislikes: 98
+# Total Accepted:    41K
+# Total Submissions: 84.7K
+# Testcase Example:  '[0,0,2,2]\n[1,1,3,3]'
+#
+# A rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) are the
+# coordinates of its bottom-left corner, and (x2, y2) are the coordinates of
+# its top-right corner.
+# 
+# Two rectangles overlap if the area of their intersection is positive.  To be
+# clear, two rectangles that only touch at the corner or edges do not overlap.
+# 
+# Given two (axis-aligned) rectangles, return whether they overlap.
+# 
+# Example 1:
+# 
+# 
+# Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+# Output: false
+# 
+# 
+# Notes:
+# 
+# 
+# Both rectangles rec1 and rec2 are lists of 4 integers.
+# All coordinates in rectangles will be between -10^9 and 10^9.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        x1,y1,x2,y2 = rec1
+        a1,b1,a2,b2 = rec2
+        return not (x1 >= a2 or x2 <= a1 or y1 >= b2 or y2 <= b1)
+        
+# @lc code=end
+
