ef8973fe6a6bb65034879d97c2e73020fa11f3cf
Lonitch <invictumltd@gmail.com>
2020-03-04 08:54:45 -0600

    3/4/20

---
+++ 844.backspace-string-compare.py
@@ -0,0 +1,92 @@
+#
+# @lc app=leetcode id=844 lang=python3
+#
+# [844] Backspace String Compare
+#
+# https://leetcode.com/problems/backspace-string-compare/description/
+#
+# algorithms
+# Easy (46.75%)
+# Likes:    1040
+# Dislikes: 61
+# Total Accepted:    101.5K
+# Total Submissions: 214.9K
+# Testcase Example:  '"ab#c"\n"ad#c"'
+#
+# Given two strings S and T, return if they are equal when both are typed into
+# empty text editors. # means a backspace character.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: S = "ab#c", T = "ad#c"
+# Output: true
+# Explanation: Both S and T become "ac".
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: S = "ab##", T = "c#d#"
+# Output: true
+# Explanation: Both S and T become "".
+# 
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: S = "a##c", T = "#a#c"
+# Output: true
+# Explanation: Both S and T become "c".
+# 
+# 
+# 
+# Example 4:
+# 
+# 
+# Input: S = "a#c", T = "b"
+# Output: false
+# Explanation: S becomes "c" while T becomes "b".
+# 
+# 
+# Note:
+# 
+# 
+# 1 <= S.length <= 200
+# 1 <= T.length <= 200
+# S and T only contain lowercase letters and '#' characters.
+# 
+# 
+# Follow up:
+# 
+# 
+# Can you solve it in O(N) time and O(1) space?
+# 
+# 
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        s = []
+        t = []
+        for c in S:
+            if c=='#' and s:
+                s.pop()
+            elif c!='#':
+                s.append(c)
+        for c in T:
+            if c=='#' and t:
+                t.pop()
+            elif c!='#':
+                t.append(c)
+        return ''.join(s)==''.join(t)
+# @lc code=end
+
