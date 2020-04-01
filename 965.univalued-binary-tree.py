5157b275f181ff1aeee0279e25b99a99ef8d1891
Lonitch <invictumltd@gmail.com>
2020-04-01 11:17:38 -0500

    4/1/20

---
+++ 965.univalued-binary-tree.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode id=965 lang=python3
+#
+# [965] Univalued Binary Tree
+#
+# https://leetcode.com/problems/univalued-binary-tree/description/
+#
+# algorithms
+# Easy (66.88%)
+# Likes:    372
+# Dislikes: 39
+# Total Accepted:    73.8K
+# Total Submissions: 109.6K
+# Testcase Example:  '[1,1,1,1,1,null,1]'
+#
+# A binary tree is univalued if every node in the tree has the same value.
+# 
+# Return true if and only if the given tree is univalued.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: [1,1,1,1,1,null,1]
+# Output: true
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [2,2,2,5,2]
+# Output: false
+# 
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# The number of nodes in the given tree will be in the range [1, 100].
+# Each node's value will be an integer in the range [0, 99].
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        lst = [root]
+        vals = []
+        while lst:
+            t = lst[0]
+            if t.left is not None:
+                lst.append(t.left)
+            if t.right is not None:
+                lst.append(t.right)
+            vals.append(t.val)
+            lst.pop(0)
+        return len(set(vals))==1
+# @lc code=end
+
